118431d50246e82fa18b44e5b413940d9c955b98
lumono <josh.zhang@live.ca>
2019-08-13 10:31:27 -0700

    init commit to github

---
+++ ardupilot-Rover-3.4/libraries/AP_HAL_Empty/Scheduler.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "AP_HAL_Empty.h"
+
+class Empty::Scheduler : public AP_HAL::Scheduler {
+public:
+    Scheduler();
+    void     init();
+    void     delay(uint16_t ms);
+    void     delay_microseconds(uint16_t us);
+    void     register_timer_process(AP_HAL::MemberProc);
+    void     register_io_process(AP_HAL::MemberProc);
+    void     suspend_timer_procs();
+    void     resume_timer_procs();
+
+    void     register_timer_failsafe(AP_HAL::Proc, uint32_t period_us);
+
+    void     system_initialized();
+
+    void     reboot(bool hold_in_bootloader);
+
+};
