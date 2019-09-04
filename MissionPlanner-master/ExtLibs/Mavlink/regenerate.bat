edece7d66324fb2aa19f423bb59f178c4f74cfc6
Fred <fredyang001@gmail.com>
2019-09-04 16:45:15 -0700

    clean project

--- MissionPlanner-master/ExtLibs/Mavlink/regenerate.bat
+++
@@ -1,6 +0,0 @@
-set PATH=c:\python27;%PATH%
-
-rd /s /q "mavlink"
-python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 "message_definitions\ardupilotmega.xml"  
-copy /y "mavlink\ardupilotmega\mavlink.cs" "Mavlink.cs"
-pause
